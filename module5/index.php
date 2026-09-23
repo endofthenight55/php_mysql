66c4c0161e4c54a9061eb3666d6e54ee7d1fa607
endofthenight55 <cocajsara@gmail.com>
2026-09-23 16:07:04 +0200

    Create index.php

---
+++ module5/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+  $dogs = array(
+       array("Daschund", "Mexico" ,20),
+       array("Husky" ,"England", 25),
+       array("Pitbull", "Siberia", 16)
+  );
+
+
+  echo "hello";
+echo $dogs[0][0] . ": Origin:" . $dogs[0][1] . " Lifespan:" . $dogs[0][2] ."<br>";
+echo $dogs[1][0] . ": Origin:" . $dogs[1][1] . " Lifespan:" . $dogs[1][2] ."<br>";
+echo $dogs[2][0] . ": Origin:" . $dogs[2][1] . " Lifespan:" . $dogs[2][2] ."<br>";
+
+  
+?>
